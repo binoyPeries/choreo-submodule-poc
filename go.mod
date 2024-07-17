59a80255fe752e3b3080adebd2410d014ed9561b
binoyPeries <binoyperies98@gmail.com>
2024-07-17 12:15:04 +0530

    Update sub module

--- go.mod
+++ go.mod
@@ -2,5 +2,6 @@ module github.com/binoyPeries/choreo-submodule-poc
 
 go 1.22.1
 
-
-require github.com/choreo-poc/submodule-repo v0.0.0-20240717054147-e057b32a5e3e
+require (
+	github.com/gorilla/mux v1.8.1
+)
--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	submodule "github.com/choreo-poc/submodule-repo"
+	submodule "github.com/binoyPeries/choreo-submodule-poc/submodule"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	submodule.Hello()
-	fmt.Println("Submodule function executed")
+	router := mux.NewRouter()
 
+	router.HandleFunc("/greeter", func(w http.ResponseWriter, r *http.Request) {
+		helloValue := submodule.Hello()
+		fmt.Fprintf(w, "Submodule function executed, response: %s", helloValue)
+
+	})
+
+	log.Println("Starting server on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
